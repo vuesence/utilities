7b0201883eb7cac5224946b9bf0ba3f82c25aabb
Ruslan Makarov <ruslan.makarov@gmail.com>
2023-08-03 15:00:07 +0700

    version checker

---
+++ src/utils/app-version-checker.ts
@@ -0,0 +1,19 @@
+export async function checkVersion(localStorageName: string) {
+  console.log("checking version");
+
+  const buildData = await fetch("/build.json?version").then((response) => {
+    return response.json();
+  });
+  console.log("checking 2");
+  if (buildData.date !== localStorage.getItem(localStorageName)) {
+    // document.location.reload();
+    console.log("updating version");
+    console.log("new version:", buildData.date);
+    localStorage.setItem(localStorageName, buildData.date);
+    window.location.href = "/?";
+    // window.location.href = `${window.location.href}?update`;
+    console.log("reloaded");
+  } else {
+    console.log("no updates");
+  }
+}
